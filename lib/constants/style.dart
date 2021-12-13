867f1d805661e9cf27aa16dbbff0157b1e1134b2
Ma'sum <muhmasum6661@gmail.com>
2021-12-13 15:57:40 +0700

    banner

---
+++ lib/constants/style.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/cupertino.dart';
+import 'package:my_travelapp/constants/color.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+var mTitleStyle = GoogleFonts.inter(
+  fontSize: 16,
+  fontWeight: FontWeight.w600,
+  color: mTitleColor,
+);
+
+var mMoreDiscountStyle = GoogleFonts.inter(
+  fontSize: 12,
+  fontWeight: FontWeight.w700,
+  color: mBlueColor,
+);
+
+var mServiceTitleStyle = GoogleFonts.inter(
+  fontSize: 12,
+  fontWeight: FontWeight.w500,
+  color: mTitleColor,
+);
+
+var mServiceSubTitleStyle = GoogleFonts.inter(
+  fontSize: 12,
+  fontWeight: FontWeight.w500,
+  color: mSubtitleColor,
+);
+
+var mPopularDestinationTitleStyle = GoogleFonts.inter(
+  fontSize: 16,
+  fontWeight: FontWeight.w500,
+  color: mCardTitleColor,
+);
+
+var mPopularDestinationSubtitleStyle = GoogleFonts.inter(
+  fontSize: 16,
+  fontWeight: FontWeight.w700,
+  color: mCardTitleColor,
+);
+
+var mTravelLogStyle = GoogleFonts.inter(
+  fontSize: 16,
+  fontWeight: FontWeight.w900,
+  color: mFillColor,
+);
+
+var mTravelLogContentStyle = GoogleFonts.inter(
+  fontSize: 10,
+  fontWeight: FontWeight.w500,
+  color: mTitleColor,
+);
+
+var mTravelLogPlaceStyle = GoogleFonts.inter(
+  fontSize: 10,
+  fontWeight: FontWeight.w500,
+  color: mBlueColor,
+);
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:my_travelapp/constants/color.dart';
+import 'package:my_travelapp/constants/style.dart';
 import 'package:my_travelapp/widgets/bottom_navigation.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:my_travelapp/models/carousel_model.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -11,6 +14,16 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _current = 0;
+
+  List<T> map<T>(List list, Function handler) {
+    List<T> result = [];
+    for (var i = 0; i < list.length; i++) {
+      result.add(handler(i, list[i]));
+    }
+    return result;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,7 +33,83 @@ class _HomePageState extends State<HomePage> {
         elevation: 0,
       ),
       backgroundColor: mBackgroundColor,
+
+      // buttom navigation
       bottomNavigationBar: BottomNavigationTravel(),
+
+      // body
+      body: Container(
+        child: ListView(
+          physics: ClampingScrollPhysics(),
+          children: <Widget>[
+            // banner section
+            Padding(
+              padding: EdgeInsets.only(left: 16, right: 16),
+              child: Text('Hi Im Pixel', style: mTitleStyle),
+            ),
+            Container(
+                alignment: Alignment.centerLeft,
+                margin: EdgeInsets.only(left: 16, right: 16),
+                width: MediaQuery.of(context).size.width,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: 190,
+                      child: Swiper(
+                        onIndexChanged: (index) {
+                          setState(() {
+                            _current = index;
+                          });
+                        },
+                        autoplay: true,
+                        layout: SwiperLayout.DEFAULT,
+                        itemCount: carousels.length,
+                        itemBuilder: (BuildContext context, index) {
+                          return Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(8),
+                              image: DecorationImage(
+                                image: AssetImage(carousels[index].image),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                    SizedBox(height: 12),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Row(
+                          children: map<Widget>(
+                            carousels,
+                            (index, image) {
+                              return Container(
+                                width: 6,
+                                height: 6,
+                                alignment: Alignment.centerLeft,
+                                margin: EdgeInsets.only(right: 8),
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: _current == index
+                                      ? mBlueColor
+                                      : mGreyColor,
+                                ),
+                              );
+                            },
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ))
+          ],
+        ),
+      ),
     );
   }
 }
